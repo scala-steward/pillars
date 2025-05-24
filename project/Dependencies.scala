570c42504c90d2008bf20f13c1d468114f20caac
Scala Steward <scala_steward@virtuslab.com>
2025-05-24 19:51:35 +0000

    chore: Update circe-core from 0.14.12 to 0.14.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object versions {
     // Dependencies
     val cats             = "2.13.0"
     val catsEffect       = "3.6.0"
-    val circe            = "0.14.12"
+    val circe            = "0.14.13"
     val circeYaml        = "0.15.3"
     val decline          = "2.5.0"
     val doobie           = "1.0.0-RC9"
