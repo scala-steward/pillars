a6095b1c17b335a3076583b24ff6b60dcf852e17
Scala Steward <scala_steward@virtuslab.com>
2026-07-28 20:32:02 +0000

    chore: Update circe-yaml from 0.15.3 to 0.16.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object versions {
     val cats             = "2.13.0"
     val catsEffect       = "3.6.0"
     val circe            = "0.14.12"
-    val circeYaml        = "0.15.3"
+    val circeYaml        = "0.16.1"
     val decline          = "2.5.0"
     val doobie           = "1.0.0-RC12"
     val flyway           = "11.5.0"
