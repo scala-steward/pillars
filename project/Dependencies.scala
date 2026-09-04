bc05c914b863182511c9d9e92f57ded31fc0c14b
Scala Steward <scala_steward@virtuslab.com>
2026-09-04 23:47:23 +0000

    chore: Update flyway-core from 11.5.0 to 11.20.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object versions {
     val circeYaml        = "0.15.3"
     val decline          = "2.6.2"
     val doobie           = "1.0.0-RC12"
-    val flyway           = "11.5.0"
+    val flyway           = "11.20.3"
     val fs2              = "3.12.2"
     val fs2Rabbit        = "5.2.0"
     val http4s           = "0.23.30"
