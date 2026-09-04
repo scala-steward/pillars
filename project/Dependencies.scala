395a3e42e55a7130e859b222bf3016c1305fb080
Scala Steward <scala_steward@virtuslab.com>
2026-09-04 23:49:53 +0000

    chore: Update munit-scalacheck from 1.1.0 to 1.3.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -29,7 +29,7 @@ object versions {
     object munit {
         val core       = "1.1.1"
         val catsEffect = "2.1.0"
-        val scalacheck = "1.1.0"
+        val scalacheck = "1.3.1"
         val http4s     = "3.0.0"
     }
 
