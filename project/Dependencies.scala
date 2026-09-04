d2ba0185ba048fcf21e770aad72b32fbf519dfa8
Scala Steward <scala_steward@virtuslab.com>
2026-09-04 23:49:48 +0000

    chore: Update munit from 1.1.0 to 1.1.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,7 +27,7 @@ object versions {
     val testContainers   = "0.43.6"
 
     object munit {
-        val core       = "1.1.0"
+        val core       = "1.1.1"
         val catsEffect = "2.1.0"
         val scalacheck = "1.1.0"
         val http4s     = "3.0.0"
